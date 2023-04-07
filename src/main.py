34bb25f69534a8f47f094d899055bed842903a92
flizzyflo <vip3r1@gmx.de>
2023-04-07 16:31:47 -0400

    refactoring

--- src/main.py
+++ src/main.py
@@ -8,37 +8,36 @@ from src.userinterface.gui import GraphicalUserInterface
 
 if __name__ == "__main__":
 
-    database_object = DatabaseManager(database_name=DATABASE_NAME,
-                                      table_name=TABLE_NAME)
+    database_object: DatabaseManager = DatabaseManager(database_name=DATABASE_NAME,
+                                                       table_name=TABLE_NAME)
+
+    continue_existing_worktime_data: str = "no"
 
     if DatabaseManager.data_already_exist(database_object=database_object):
-        year, month, day = str(datetime.date.today()).split("-")
+        current_date: str = str(datetime.date.today())
+        year, month, day = current_date.split("-")
         fetched_data = database_object.fetch_single_entry(year_filter=year,
                                                           month_filter=month,
-                                                          day_filter=day)
+                                                          day_filter=day)[0]
 
-        fetched_data = fetched_data[0]
+        # grab hours, minutes and seconds from data tuple
         stored_hours, stored_minutes, stored_seconds = fetched_data[3:]
 
-        answer = messagebox.askquestion(title="Continue existing data",
-                                        message=f"""Already stored data for today.
+        continue_existing_worktime_data = messagebox.askquestion(title="Continue existing data",
+                                                                 message=f"""Already stored data for today.
                                                 \nDo you want to continue existing data?
-                                                Stored data is: {int(stored_hours):02.0f} : {int(stored_minutes):02.0f} 
+                                                Stored data is: {int(stored_hours):02.0f} : {int(stored_minutes):02.0f}
                                                 : {int(stored_seconds):02.0f} (hh:mm:ss).
                                                 \nIf 'No', it will be overriden.""")
 
-        if answer == "yes":
+        if continue_existing_worktime_data == "yes":
 
             clock = WorkTimeClock(initial_hours=int(stored_hours),
                                   initial_minutes=int(stored_minutes),
                                   initial_seconds=int(stored_seconds))
 
-        elif answer == "no":
-            clock = WorkTimeClock(initial_hours=0,
-                                  initial_minutes=0,
-                                  initial_seconds=0)
-
-    else:
+    # no information stored according current date or no desire to continue existing information
+    if not DatabaseManager.data_already_exist(database_object=database_object) or continue_existing_worktime_data == "no":
         clock = WorkTimeClock(initial_hours=0,
                               initial_minutes=0,
                               initial_seconds=0)
