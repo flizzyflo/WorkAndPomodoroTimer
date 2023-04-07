5416f6275fbbc8e9e167cf62d8c079b2f09fe42a
flizzyflo <vip3r1@gmx.de>
2023-04-06 21:47:00 -0400

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 
-from Clocks.Clock import Clock
-from UserInterface.GrapicalUserInterface import GraphicalUserInterface
-from WorkTimeDatabase.DatabaseManager import DatabaseManager
-from Clocks.PomodoroClock import PomodoroClock
-from Settings.Settings import LONGBREAK, SHORTBREAK, DATABASE_NAME, TABLE_NAME
+from src.clocks.worktimeclock import WorkTimeClock
+from src.database.databasemanager import DatabaseManager
+from src.settings.settings import DATABASE_NAME, TABLE_NAME
+from src.userinterface.gui import GraphicalUserInterface
 
 if __name__ == "__main__":
 
-    database_object = DatabaseManager(database_name= DATABASE_NAME, table_name= TABLE_NAME)
-    clock = Clock()
+    database_object = DatabaseManager(database_name=DATABASE_NAME,
+                                      table_name=TABLE_NAME)
+    clock = WorkTimeClock(initial_hours=0,
+                          initial_minutes=0,
+                          initial_seconds=0)
+
     # pomodoro = PomodoroClock(24, 60, SHORTBREAK, LONGBREAK)
-    gui = GraphicalUserInterface(clock_object=clock, database_object= database_object)
+
+    gui = GraphicalUserInterface(clock_object=clock,
+                                 database_object=database_object)
     
     gui.mainloop()
