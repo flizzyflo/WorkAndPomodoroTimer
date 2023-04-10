ea25d9d5a0b9426a21b4e0fa06ec3f073a449b58
flizzyflo <vip3r1@gmx.de>
2023-04-11 00:28:41 +0200

    implemented work_time_information_tuple.py as namedtuple class for worktime information

--- src/database/work_time_information_tuple.py
+++ src/database/work_time_information_tuple.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 from src.settings.settings import DATABASE_INFORMATION_FIELDS
 
 
-class WorkTimeTuple(namedtuple(typename="WorkTimeTuple",
-                               field_names=DATABASE_INFORMATION_FIELDS)):
+class WorkTimeInformationTuple(namedtuple(typename="WorkTimeTuple",
+                                          field_names=DATABASE_INFORMATION_FIELDS)):
 
     def __repr__(self) -> str:
         return f"{self.year}-{self.month}-{self.day} | {int(self.hours):02.0f}:{int(self.minutes):02.0f}:{int(self.seconds):02.0f}"
