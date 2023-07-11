b44024f8c85afbe70c90a640d5fdca7df3415753
flizzyflo <vip3r1@gmx.de>
2023-07-11 16:59:12 +0200

    minor changes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,16 @@
 
 from src.clock.worktimeclock import WorkTimeClock
-
-
-from src.menu_information_management.menuinformation import MenuInformation
+from src.database.database_connector import DatabaseConnector
 from src.graphical_user_interface.graphicaluserinterface import GraphicalUserInterface
 
 
 if __name__ == "__main__":
 
-    menu_information = MenuInformation()
-
+    #database_connector = DatabaseConnector.get_database_connection(database_path="to_be_passed_in")
     clock = WorkTimeClock(initial_hours=0,
                           initial_minutes=00,
                           initial_seconds=0)
 
-    continue_existing_worktime_data: str = "no"
 
     gui = GraphicalUserInterface(work_time_clock=clock)
     
