f5a061686d1c4c2cc9c2e070aecd7f476a267ff1
flizzyflo <vip3r1@gmx.de>
2023-07-12 21:57:38 +0200

    implemented database reader and export methods

--- src/database/database_reader.py
+++ src/database/database_reader.py
@@ -1,4 +1,23 @@
+import sqlite3
+import csv
+from pathlib import Path
+import os
 
 
 class DatabaseReader:
-    pass
+
+    def __init__(self, *, database_connection: sqlite3.Connection):
+        self.database_connection: sqlite3.Connection = database_connection
+        self.cursor: sqlite3.Cursor = self.database_connection.cursor()
+
+    def __read_database_entries(self) -> list:
+        return self.cursor.execute("""SELECT * FROM worktime_documentation""").fetchall()
+
+    def _export_database_entries(self, filepath: str | Path) -> None:
+        all_database_entries: list = self.__read_database_entries()
+        export_file_path: Path = Path(os.path.join(filepath, "export.csv"))
+        with open(os.path.join(export_file_path), "w") as exported_worktime_file:
+            file = csv.writer(exported_worktime_file, delimiter=";")
+            file.writerow(["date", "daily_work_time"])
+            for entry in all_database_entries:
+                file.writerow(entry)
