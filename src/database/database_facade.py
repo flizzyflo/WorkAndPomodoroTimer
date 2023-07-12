45ab3543c4540d0f2bfa1419175a498bdfc8be58
flizzyflo <vip3r1@gmx.de>
2023-07-12 21:58:28 +0200

    initially created facade object for interacting with database via gui

---
+++ src/database/database_facade.py
@@ -0,0 +1,25 @@
+import sqlite3
+from pathlib import Path
+import csv
+from src.database.database_writer import DatabaseWriter
+from src.database.database_reader import DatabaseReader
+from src.database.database_connector import DatabaseConnector
+
+
+class DatabaseFacade:
+
+    def __init__(self, *, database_path: str | Path) -> None:
+        self.database_connection: sqlite3.Connection = DatabaseConnector.get_database_connection(database_path=database_path)
+        self.database_writer: DatabaseWriter = DatabaseWriter(database_connection=self.database_connection)
+        self.database_reader: DatabaseReader = DatabaseReader(database_connection=self.database_connection)
+
+    def insert_data_into_database(self, worktime: str) -> None:
+        self.database_writer.insert_entry_to_database(worktime=worktime)
+
+    def entry_for_current_date_already_exists(self) -> bool:
+        return self.database_writer._entry_exists_in_database()
+
+    def export_database_entries(self, *, filepath: str | Path) -> None:
+        return self.database_reader._export_database_entries(filepath=filepath)
+
+    # TODO add menubar to select a path and start the export via button.
