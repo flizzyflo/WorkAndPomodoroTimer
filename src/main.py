e3c4fffa8cb98565b7caa1a3c3c12322c19f2978
flizzyflo <vip3r1@gmx.de>
2023-07-13 18:24:37 +0200

    -

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ from src.database.database_facade import DatabaseFacade
 
 if __name__ == "__main__":
 
-    database_accessor = DatabaseFacade(database_path=r"/Applications/ProgrammingFiles/Python/Published/WorkTimer/src/database/test2.db")
+    DATABASE_PATH: str = r"/Applications/ProgrammingFiles/Python/Published/WorkTimer/src/database/test2.db"
+
+    database_accessor = DatabaseFacade(database_path=DATABASE_PATH)
     clock = WorkTimeClock(initial_hours=0,
                           initial_minutes=00,
                           initial_seconds=0)
