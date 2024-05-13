28f51e0459d185a4c5e4549b54ec4ad04be1a1aa
flizzyflo <vip3r1@gmx.de>
2024-05-13 11:16:37 +0200

    made get_database_connection more robust

--- src/database/database_connector.py
+++ src/database/database_connector.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 
 class DatabaseConnector:
-
     """
     Factory class to return the connection for the specific database desired.
     """
 
     @staticmethod
-    def get_database_connection(*, database_path: str | Path) -> sqlite3.Connection:
+    def get_database_connection(*, database_path: Path) -> sqlite3.Connection:
 
         """
         Connects to the database and returns the Connection object to the database to work with later on.
@@ -20,10 +19,15 @@ class DatabaseConnector:
         :return: sqlite3.Connection object to the database passed in as path
         """
 
-        if not DatabaseConnector.path_exists(path=database_path):
-            raise FileNotFoundError
+        db_connection: sqlite3.Connection
 
-        return sqlite3.connect(database=database_path)
+        if not database_path.exists():
+            db_connection = sqlite3.connect(database=database_path)
+            DatabaseConnector.create_database_table(database_path=database_path)
+        else:
+            db_connection = sqlite3.connect(database=database_path)
+
+        return db_connection
 
     @staticmethod
     def path_exists(*, path: str | Path) -> bool:
@@ -33,22 +37,14 @@ class DatabaseConnector:
         return os.path.exists(path)
 
     @staticmethod
-    def create_database(*, database_path: str | Path, database_name: str) -> None:
-
-        if not DatabaseConnector.path_exists(path=database_path):
-            raise FileNotFoundError
-
-        full_database_path: str = str(os.path.join(database_path, database_name))
-        if not full_database_path.endswith(".db"):
-            full_database_path: Path = Path(full_database_path + ".db")
-
-        sqlite3.connect(database=full_database_path)
-
-    @staticmethod
     def create_database_table(database_path: str | Path) -> None:
         cursor: sqlite3.Cursor = DatabaseConnector.get_database_connection(database_path=database_path).cursor()
 
-        cursor.execute("""CREATE TABLE IF NOT EXISTS worktime_documentation
-          ([date] date PRIMARY KEY, [work_time] TEXT)
-        """)
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS worktime_documentation
+            ([date] date PRIMARY KEY, 
+            [work_time] TEXT)
+            """)
 
+        cursor.close()
