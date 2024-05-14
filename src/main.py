ab0f7b36041d39c8fcf5002e46167560bd947b82
flizzyflo <vip3r1@gmx.de>
2024-05-14 19:30:21 +0200

    removed database path

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 
+from pathlib import Path
 from src.clock.worktimeclock import WorkTimeClock
 from src.graphical_user_interface.graphicaluserinterface import GraphicalUserInterface
 from src.database.database_facade import DatabaseFacade
@@ -6,14 +7,12 @@ from src.database.database_facade import DatabaseFacade
 
 if __name__ == "__main__":
 
-    DATABASE_PATH: str = r"/Applications/ProgrammingFiles/Python/Published/WorkTimer/src/database/test2.db"
-
-    database_accessor = DatabaseFacade(database_path=DATABASE_PATH)
+    database_accessor = DatabaseFacade()
     clock = WorkTimeClock(initial_hours=0,
                           initial_minutes=00,
                           initial_seconds=0)
 
     gui = GraphicalUserInterface(work_time_clock=clock,
                                  database_facade=database_accessor)
-    
+
     gui.mainloop()
