431dd9d39ef97901ae55e1cdc4c4aca5c7cc9778
flizzyflo <vip3r1@gmx.de>
2024-05-27 14:21:07 +0200

    initial creation of menubarmanager class

--- src/graphical_user_interface/menubar_manager.py
+++ src/graphical_user_interface/menubar_manager.py
@@ -1,3 +1,18 @@
+import tkinter as tk
 
-class MenuBarManager:
-    pass
+
+class MenuBarManager(tk.Menu):
+    """
+    Base Menuclass to handle Menu and allow the user to
+    use several functionalities.
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.filemenu = tk.Menu(master=self)
+        self.add_cascade(label="File", menu=self.filemenu)
+        self.filemenu.add_command(label="Export file")
+        self.filemenu.add_command(label="Settings")
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Quit", command=lambda: quit())
+
+
