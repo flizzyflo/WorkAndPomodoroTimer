7f330c101fe1de47fdbcfd33d861930c438d6d01
flizzyflo <vip3r1@gmx.de>
2024-05-28 18:16:32 +0200

    initial creation of export functionality via menu

--- src/graphical_user_interface/menubar_manager.py
+++ src/graphical_user_interface/menubar_manager.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+from src.database.database_facade import DatabaseFacade
 
 
 class MenuBarManager(tk.Menu):
@@ -10,9 +12,15 @@ class MenuBarManager(tk.Menu):
         super().__init__(*args, **kwargs)
         self.filemenu = tk.Menu(master=self)
         self.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="Export file")
+        self.filemenu.add_command(label="Export file", command=lambda: self.export_database_information())
         self.filemenu.add_command(label="Settings")
         self.filemenu.add_separator()
         self.filemenu.add_command(label="Quit", command=lambda: quit())
+        self.db_facade: DatabaseFacade = None
 
+    def export_database_information(self) -> None:
+        self.db_facade = DatabaseFacade()
+        path_to_store_file: str = filedialog.askdirectory(title="Save export to...")
+        self.db_facade.export_database_entries(path_to_store_csv_file=path_to_store_file)
+        self.db_facade = None
 
