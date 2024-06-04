6c630a87e4d5f587e8e021c86ae39991df1ba63c
flizzyflo <vip3r1@gmx.de>
2024-06-04 22:01:39 +0200

    added methods to settingsmenu.py buttons for quitting and saving changes

--- src/graphical_user_interface/settingsmenu.py
+++ src/graphical_user_interface/settingsmenu.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 from typing import Dict
 
@@ -17,17 +16,16 @@ class SettingsMenu(tk.Tk):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.entry_frame = tk.Frame(master= self)
+        self.entry_frame = tk.Frame(master=self)
         self.entry_frame.pack()
         self.initialize_entry_widgets()
 
-        self.save_button = tk.Button(master=self, text="Save", command=lambda: quit())
+        self.save_button = tk.Button(master=self, text="Save", command=lambda: self.save_settings())
         self.save_button.pack(fill=tk.BOTH)
-        self.quit_button = tk.Button(master=self, text="Quit", command=lambda: quit())
+        self.quit_button = tk.Button(master=self, text="Quit", command=lambda: self.destroy())
         self.quit_button.pack(fill=tk.BOTH)
 
     def initialize_entry_widgets(self) -> None:
-
         work_time_settings = read_from_json("work_times.json")
         print(work_time_settings)
         tk.Label(master=self.entry_frame, text="Usual daily hours: ").grid(column=0, row=0)
@@ -50,3 +48,15 @@ class SettingsMenu(tk.Tk):
         self.max_worktime_minutes.insert(0, str(work_time_settings["MAX_DAILY_WORK_TIME_MINUTES"]))
         self.max_worktime_minutes.grid(column=1, row=4)
 
+    def save_settings(self) -> None:
+        new_settings = {
+            "NORMAL_DAILY_WORK_TIME_HOURS": self.normal_worktime_hours.get(),
+            "NORMAL_DAILY_WORK_TIME_MINUTES": self.normal_worktime_minutes.get(),
+            "NORMAL_DAILY_WORK_TIME_SECONDS": 0,
+            "MAX_DAILY_WORK_TIME_HOURS": self.max_worktime_hours.get(),
+            "MAX_DAILY_WORK_TIME_MINUTES": self.max_worktime_minutes.get(),
+            "MAX_DAILY_WORK_TIME_SECONDS": 0
+        }
+
+        write_to_json("work_times.json", new_settings)
+        self.destroy()
