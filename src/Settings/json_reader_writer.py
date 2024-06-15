790b04b8393317203efbd794f5a50e39ae9bb2ab
flizzyflo <vip3r1@gmx.de>
2024-06-15 16:06:56 +0200

    added docstring

--- src/Settings/json_reader_writer.py
+++ src/Settings/json_reader_writer.py
@@ -5,6 +5,15 @@ from pathlib import Path
 
 
 def read_from_json(filename: str) -> dict[str, int]:
+    """
+    Method  to read the contents of the json-file passed in as filename.
+    Args:
+        filename: filename of the json file to be read
+
+    Returns:
+        dict[str, str: returns the json content as a python dictionary
+
+    """
     settings_file_path: Path = Path(os.path.join(os.path.dirname(__file__), filename))
     with open(settings_file_path, "r") as file_to_read:
         json_content = json.load(file_to_read)
@@ -13,6 +22,12 @@ def read_from_json(filename: str) -> dict[str, int]:
 
 
 def write_to_json(filename: str, content_to_write: dict[str, str]) -> None:
+    """
+    Method to write settings to the filename passed in as argument. Writes the dictionary content to the file.
+    Args:
+        filename: filename of the json settings file to write to
+        content_to_write: dictionary storing the values to save to json file
+    """
     settings_file_path: Path = Path(os.path.join(os.path.dirname(__file__), filename))
     j = json.dumps(content_to_write)
 
