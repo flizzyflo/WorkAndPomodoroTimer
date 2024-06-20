7cae9732fd25d2f106024a350110fcff6abd9fe0
flizzyflo <vip3r1@gmx.de>
2024-06-20 18:29:32 +0200

    refactored for better readability

--- src/graphical_user_interface/menubar_manager.py
+++ src/graphical_user_interface/menubar_manager.py
@@ -17,20 +17,26 @@ class MenuBarManager(tk.Menu):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.filemenu = tk.Menu(master=self)
-        self.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="Export work time", command=lambda: self.export_database_information())
-        self.filemenu.add_command(label="Settings", command=lambda: SettingsMenu())
+        self.add_cascade(label="File",
+                         menu=self.filemenu)
+        self.filemenu.add_command(label="Export work time",
+                                  command=lambda: self.export_database_information())
+        self.filemenu.add_command(label="Settings",
+                                  command=lambda: SettingsMenu())
         self.filemenu.add_separator()
-        self.filemenu.add_command(label="Quit", command=lambda: quit())
+        self.filemenu.add_command(label="Quit",
+                                  command=lambda: quit())
         self.information_menu = tk.Menu(master=self)
         self.add_cascade(label="Information", menu=self.information_menu)
-        self.information_menu.add_command(label="Application information", command= lambda: MenuInformation.show_menubar_information())
-        self.information_menu.add_command(label="Version information", command= lambda: MenuInformation.show_version_information(PROGRAM_VERSION))
+        self.information_menu.add_command(label="Application information",
+                                          command=lambda: MenuInformation.show_menubar_information())
+        self.information_menu.add_command(label="Version information",
+                                          command=lambda: MenuInformation.show_version_information(PROGRAM_VERSION))
         self.db_facade = None
 
     def export_database_information(self) -> None:
         self.db_facade = DatabaseFacade()
-        path_to_store_file: str = filedialog.askdirectory(title="Save export to...")
+        path_to_store_file: str = filedialog.askdirectory(title="Select destination path")
         self.db_facade.export_database_entries(path_to_store_csv_file=path_to_store_file)
         self.db_facade = None
 
